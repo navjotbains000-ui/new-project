d99b0885dba500c826e2887bf96be38259b9e5e7
navjotbains000-ui <navjotbains000@gmail.com>
2026-05-27 17:14:06 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -6,3 +6,8 @@ resource "azurerm_resource_group" "rg2" {
   name     = "rg2"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "rg3" {
+  name     = "rg3"
+  location = "West Europe"
+}
+
